8f30621519b205fecc000d321d8343ef76306aa8
Alice <xinranjiang2015@yahoo.com>
2023-11-27 14:56:34 -0500

    some fixes on VPR, added new vpr script

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from vis_nav_game import Player, Action
+from vis_nav_game import Player, Action, Phase
 import pygame
 import cv2
 import math
@@ -6,6 +6,8 @@ import threading
 import queue
 import numpy as np
 from config import update_config
+from vpr_new import generate_histogram, train_vocab, best_match
+
 
 class KeyboardPlayerPyGame(Player):
     def __init__(self):
@@ -14,10 +16,10 @@ class KeyboardPlayerPyGame(Player):
         self.screen = None
         self.keymap = None
         super(KeyboardPlayerPyGame, self).__init__()
-        
-        self.arrow_pos = np.array([300, 300])
-        self.arrow_angle = 90  # Angle in degrees, 90 means pointing upwards
-        self.arrow_trail = []  # List to store the positions of the arrow
+        self.kmeans = None
+        # each element is a {histogram, position} pair
+        self.live_image_list = []
+        self.counter = 0
 
     def reset(self):
         self.fpv = None
@@ -32,41 +34,40 @@ class KeyboardPlayerPyGame(Player):
             pygame.K_SPACE: Action.CHECKIN,
             pygame.K_ESCAPE: Action.QUIT
         }
-    
 
-    def pre_exploration(self): 
-        # Update YAML file with K matrix 
-        K = self.get_camera_intrinsic_matrix() 
-        print(type(K))
-        if K is not None:
-            update_config('test.yaml', K)
-        # Initialize SLAM 
+    def pre_exploration(self):
+        if self.kmeans is not None:
+            return  # already trained
 
-    
-    def show_additional_window(self):
-        """
-        Display an additional window with a green arrow.
-        The direction of the arrow is based on the last action.
-        """
-        # Creating a black canvas
-        canvas = np.zeros((600, 600, 3), dtype=np.uint8)
+        print('pre exploration phase ...')
+        # Update YAML file with K matrix
+        K = self.get_camera_intrinsic_matrix()
+        # print(type(K))
+        # if K is not None:
+        #     update_config('test.yaml', K)
 
-        # Drawing the trail
-        for i in range(1, len(self.arrow_trail)):
-            cv2.line(canvas, tuple(self.arrow_trail[i-1]), tuple(self.arrow_trail[i]), (0, 255, 0), 2)
-        
-               
-        # Calculate the tip of the arrow based on position and angle
-        tip_x = self.arrow_pos[0] + 10 * math.cos(math.radians(self.arrow_angle))
-        tip_y = self.arrow_pos[1] - 10 * math.sin(math.radians(self.arrow_angle))
+        # build visual vocab
+        kmeans = train_vocab()
+        self.kmeans = kmeans
+        print('done updating kmeans, now entering pre navigation phase ...')
 
-        # Drawing the arrow
-        cv2.arrowedLine(canvas, tuple(self.arrow_pos), (int(tip_x), int(tip_y)), (0, 255, 0), 5)
-
-        # Displaying the canvas
-        cv2.imshow('Additional Window', canvas)
-        cv2.waitKey(1)
-
+    def pre_navigation(self):
+        print('pre navigation phase begins...')
+        # find the best match from the live image list
+        hist_list = [item['hist'] for item in self.live_image_list]
+        target_images = self.get_target_images()
+        kmeans = self.kmeans
+        if hist_list is None or len(hist_list) <= 0:
+            print('No live images found.')
+            return
+        print("found live images: ", len(hist_list))
+        best_match_indices = best_match(target_images, kmeans, hist_list)
+        print('Best match indices: ', best_match_indices)
+        for i in best_match_indices:
+            image = self.live_image_list[i]['image']
+            # show image in another window
+            cv2.imshow(f'Best Match #{i}', image)
+            cv2.waitKey(1)
 
     def act(self):
         for event in pygame.event.get():
@@ -84,49 +85,7 @@ class KeyboardPlayerPyGame(Player):
                 if event.key in self.keymap:
                     self.last_act ^= self.keymap[event.key]
 
-        move_step = 5
-        rotate_step = 2.5
-          
-        if self.last_act & Action.FORWARD:
-            self.arrow_pos[0] += move_step * math.cos(math.radians(self.arrow_angle)) 
-            self.arrow_pos[1] -= move_step * math.sin(math.radians(self.arrow_angle))
-        elif self.last_act & Action.BACKWARD:
-            self.arrow_pos[0] -= move_step * math.cos(math.radians(self.arrow_angle)) 
-            self.arrow_pos[1] -= move_step * math.sin(math.radians(-self.arrow_angle))
-  
-
-
-        if self.last_act & Action.LEFT:
-            self.arrow_angle += rotate_step
-        elif self.last_act & Action.RIGHT:
-            self.arrow_angle -= rotate_step
-        
-        #self.arrow_angle %= 360  # Keep the angle within 0 to 359 degrees
-
-        # Add the new position to the trail
-        self.arrow_trail.append(self.arrow_pos.copy())
-
-        self.show_additional_window()
         return self.last_act
-    
-    def match_features(self, img1, img2):
-        """Match features between two images."""
-        orb = cv2.ORB_create()
-
-        # Find the keypoints and descriptors with ORB
-        kp1, des1 = orb.detectAndCompute(img1, None)
-        kp2, des2 = orb.detectAndCompute(img2, None)
-
-        # Create a brute force matcher object
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-        # Match descriptors
-        matches = bf.match(des1, des2)
-
-        # Sort them in ascending order of distance
-        matches = sorted(matches, key=lambda x: x.distance)
-
-        return len(matches)
 
     def show_target_images(self):
         targets = self.get_target_images()
@@ -137,11 +96,13 @@ class KeyboardPlayerPyGame(Player):
         concat_img = cv2.vconcat([hor1, hor2])
 
         w, h = concat_img.shape[:2]
-        
+
         color = (0, 0, 0)
 
-        concat_img = cv2.line(concat_img, (int(h/2), 0), (int(h/2), w), color, 2)
-        concat_img = cv2.line(concat_img, (0, int(w/2)), (h, int(w/2)), color, 2)
+        concat_img = cv2.line(concat_img, (int(h/2), 0),
+                              (int(h/2), w), color, 2)
+        concat_img = cv2.line(concat_img, (0, int(w/2)),
+                              (h, int(w/2)), color, 2)
         w_offset = 25
         h_offset = 10
         font = cv2.FONT_HERSHEY_SIMPLEX
@@ -149,12 +110,14 @@ class KeyboardPlayerPyGame(Player):
         size = 0.75
         stroke = 1
 
-        cv2.putText(concat_img, 'Front View', (h_offset, w_offset), font, size, color, stroke, line)
-        cv2.putText(concat_img, 'Right View', (int(h/2) + h_offset, w_offset), font, size, color, stroke, line)
-        cv2.putText(concat_img, 'Back View', (h_offset, int(w/2) + w_offset), font, size, color, stroke, line)
-        cv2.putText(concat_img, 'Left View', (int(h/2) + h_offset, int(w/2) + w_offset), font, size, color, stroke, line)
-
-
+        cv2.putText(concat_img, 'Front View', (h_offset, w_offset),
+                    font, size, color, stroke, line)
+        cv2.putText(concat_img, 'Right View', (int(h/2) + h_offset,
+                    w_offset), font, size, color, stroke, line)
+        cv2.putText(concat_img, 'Back View', (h_offset, int(
+            w/2) + w_offset), font, size, color, stroke, line)
+        cv2.putText(concat_img, 'Left View', (int(h/2) + h_offset,
+                    int(w/2) + w_offset), font, size, color, stroke, line)
 
         cv2.imshow(f'KeyboardPlayer:target_images', concat_img)
         cv2.waitKey(1)
@@ -163,29 +126,33 @@ class KeyboardPlayerPyGame(Player):
         super(KeyboardPlayerPyGame, self).set_target_images(images)
         self.show_target_images()
 
+    def get_position(self):
+        return (0, 0, 0)  # TODO: get current position on the map
+
+    def build_hist(self, fpv):
+        print('process captured images ...')
+        if self.kmeans is not None:
+            hist = generate_histogram(fpv, self.kmeans)
+            self.live_image_list.append(
+                {'hist': hist, 'image': fpv, 'position': self.get_position})
+
     def see(self, fpv):
         if fpv is None or len(fpv.shape) < 3:
             return
 
         self.fpv = fpv
+        self.counter += 1
 
-        targets = self.get_target_images()
-        
-        state = self.get_state() 
-        # print(state)
-        print(self.get_camera_intrinsic_matrix())
+        if self.counter % 10 == 0:
+            # !Run build_hist every 5 cycles (0.5 second) this might be too slow
+            self.build_hist(fpv)
 
-        target_names = ["Front", "Left", "Back", "Right"]            
-      
-        for index, target in enumerate(targets):
-            match_score = self.match_features(fpv, target)
-
-            # Check if the match score meets the threshold
-            if match_score > 147:  # Using a threshold of 148                
-                # Display the match score for each target on the FPV with corresponding names
-                cv2.putText(self.fpv, f'{target_names[index]} View, Score: {match_score}', 
-                    (40, 40 ),  # Adjust position based on target index
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+        state = self.get_state()
+        if state is not None:
+            if state == Phase.EXPLORATION:  # in the NAV phase, we generate histogram for each captured image
+                # ! for some reason this does not run at all in the exploration phase
+                print('EXPLOREATION!')
+                pass
 
         if self.screen is None:
             h, w, _ = fpv.shape
@@ -198,8 +165,10 @@ class KeyboardPlayerPyGame(Player):
             see https://blanktar.jp/blog/2016/01/pygame-draw-opencv-image.html
             """
             opencv_image = opencv_image[:, :, ::-1]  # BGR->RGB
-            shape = opencv_image.shape[1::-1]  # (height,width,Number of colors) -> (width, height)
-            pygame_image = pygame.image.frombuffer(opencv_image.tobytes(), shape, 'RGB')
+            # (height,width,Number of colors) -> (width, height)
+            shape = opencv_image.shape[1::-1]
+            pygame_image = pygame.image.frombuffer(
+                opencv_image.tobytes(), shape, 'RGB')
 
             return pygame_image
 
---
+++ vpr_new.py
@@ -0,0 +1,138 @@
+import numpy as np
+from sklearn.cluster import KMeans
+import cv2
+import os
+
+
+# Util function to load all images from folder
+def load_images_from_folder(folder_path):
+
+    images = []
+    # List all image files in the folder
+    image_files = [f for f in os.listdir(
+        folder_path) if f.endswith('.jpg') or f.endswith('.png')]
+
+    for image_file in image_files:
+        image_path = os.path.join(folder_path, image_file)
+
+        # Read and append each image to the 'images' list
+        img = cv2.imread(image_path)
+        if img is not None:
+            images.append(img)
+
+    return images
+
+
+def find_feature_points(images):
+    # ORB feature detection
+    orb = cv2.ORB_create()
+    kp = []
+    des = []
+    des_single_list = []
+    # Detect keypoints and compute descriptors for each image
+    for img in images:
+        kp_img, des_img = orb.detectAndCompute(img, None)
+        kp.append(kp_img)
+        des.append(des_img)
+        des_single_list.extend(des_img)
+
+    return kp, des, des_single_list
+
+
+# features is a single list of descriptors of all images
+def build_vocabulary(features, vocab_size):
+    # clustering
+    kmeans = KMeans(n_clusters=vocab_size, random_state=0)
+    kmeans.fit(features)
+    visual_words = kmeans.cluster_centers_
+
+    # Return the cluster centers (visual words)
+    return visual_words, kmeans
+
+
+# generate a list of histograms of visual words for images
+def generate_feature_histograms(descriptors, kmeans):
+    num_clusters = kmeans.cluster_centers_.shape[0]
+    histograms = []
+
+    for desc in descriptors:
+        histogram = np.zeros(num_clusters)
+        labels = kmeans.predict(desc)
+        for label in labels:
+            histogram[label] += 1
+        histograms.append(histogram)
+
+    return histograms
+
+# input 1: histograph of query image
+# input 2: the list of histograms of all images in the dataset
+
+
+def compare_histograms(query_histogram, list_of_histograms):
+    # Calculate Euclidean distances
+    distances = [np.linalg.norm(query_histogram - hist)
+                 for hist in list_of_histograms]
+
+    # Find the index of the most similar histogram
+    most_similar_index = np.argmin(distances)
+
+    return most_similar_index
+
+
+def process_image_and_find_best_match(kmeans, new_image, list_of_histograms):
+    # Step 1: Extract features from the new image
+    orb = cv2.ORB_create()
+    _, descriptors = orb.detectAndCompute(new_image, None)
+
+    # Step 2: Generate the feature histogram for the new image
+    num_clusters = kmeans.cluster_centers_.shape[0]
+    histogram = np.zeros(num_clusters)
+    labels = kmeans.predict(descriptors)
+    for label in labels:
+        histogram[label] += 1
+
+    # Step 3: Compare the histogram to the list of histograms
+    most_similar_index = compare_histograms(histogram, list_of_histograms)
+
+    # ? Alternatively, Find the indices of the 5 best candidates
+    # distances = [np.linalg.norm(histogram - hist) for hist in list_of_histograms]
+    # best_candidates_indices = np.argsort(distances)[:5]
+
+    return most_similar_index
+
+# ====================================================================================================
+
+
+def train_vocab():
+    print('Loading textures...')
+    folder_path = './data/textures/'
+    images = load_images_from_folder(folder_path)
+    print(len(images), 'textures loaded.')
+
+    print('Finding feature points...')
+    keypoints, descriptors, descriptors_list = find_feature_points(images)
+    print('Building visual dictionary...')
+    visual_dictionary, kmeans = build_vocabulary(descriptors_list, 100)
+    print('Visual dictionary built.')
+    return kmeans
+
+# histogram of a single image
+
+
+def generate_histogram(image, kmeans):
+    orb = cv2.ORB_create()
+    _, descriptors = orb.detectAndCompute(image, None)
+    histogram = generate_feature_histograms([descriptors], kmeans)
+    return histogram
+
+# find best match
+
+
+def best_match(target_images, kmeans, histograms):
+    best_match_indices = []
+    for target_image in target_images:
+        print('Finding best match...')
+        best_match_index = process_image_and_find_best_match(
+            kmeans, target_image, histograms)
+        best_match_indices.append(best_match_index)
+    return best_match_indices
